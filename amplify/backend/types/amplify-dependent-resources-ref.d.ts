273785e317eab737f1df2a8962a19ce557a4c03c
ahb0226@gmail.com <ahb0226@gmail.com>
2024-04-02 15:44:56 +0900

    start

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "instantripui": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "function": {
+    "instantripui452cdcab": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/instantrip_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.instantrip_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
